88556f102e7bff188cdb305140cdfeba45e14d06
Brian <brhiln@gmail.com>
2019-02-13 08:40:11 -0800

    Remove: unneccessary imports

--- src/RenderEngineBase/PauseMenu.java
+++ src/RenderEngineBase/PauseMenu.java
@@ -4,10 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class PauseMenu extends Menu {
 	
